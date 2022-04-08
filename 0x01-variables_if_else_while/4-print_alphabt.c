d73cc26398963e935186e0761664d642f21ee476
ALASHI1 <alashimuyiwa@gmail.com>
2022-04-08 16:16:33 -0700

    if/else

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - A program that prints all the alphabet except
+ * q and e in lowercase followed by a new line
  *
- * Return: Always 0 for success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int letter;
 
-	/* Print alphabet in lowercase */
+	/* Start from a */
 	letter = 'a';
+
 	while (letter <= 'z')
 	{
-		if (letter != 'q' || letter != 'e')
+		if (letter == 'q' || letter == 'e')
 		{
 			letter++;
+			continue;
 		}
 		putchar(letter);
+		letter++;
 	}
-
 	putchar('\n');
 	return (0);
 }
