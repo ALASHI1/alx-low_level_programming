acbaa711dd391f72cf5af1332232ced2d366ac07
ALASHI1 <alashimuyiwa@gmail.com>
2022-04-12 13:38:52 -0700

    loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char ch;
+	int num = 0;
+	while (num<10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+		num++;
+	}
+}
