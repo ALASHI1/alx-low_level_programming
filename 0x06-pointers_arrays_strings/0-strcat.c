f9847e380fc077ecf828db0175b98db7c1584b79
ALASHI1 <alashimuyiwa@gmail.com>
2022-04-24 13:22:00 -0700

    pointers_arrays_strings/

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - to join two strings
+ *
+ * @dest - str1
+ *
+ * @src - srt2
+ *
+ * Returns - pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lenSrc = 0, lenDest = 0;
+
+	while (*(dest + lenDest) != '\0')
+		lenDest++;
+
+	/* While true */
+	while (1 == 1)
+	{
+		*(dest + lenDest) = *(src + lenSrc);
+		if (*(src + lenSrc) == '\0')
+			break;
+
+		lenSrc++;
+		lenDest++;
+	}
+
+	return (dest);
+}
