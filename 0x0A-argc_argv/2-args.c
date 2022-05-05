20b259178e7c6b858a3bf258f4c4a7f99b4f5803
ALASHI1 <alashimuyiwa@gmail.com>
2022-05-05 08:39:50 -0700

    argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print arguments passed to an array line by line
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+
